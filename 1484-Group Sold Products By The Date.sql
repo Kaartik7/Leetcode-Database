9b97db699355444cfbdfac4d3485a1e6d2ca9934
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2023-02-18 12:13:40 -0500

    Delete 1484-Group Sold Products By The Date.sql

--- 1484-Group Sold Products By The Date.sql
+++
@@ -1,3 +0,0 @@
-select sell_date, count(distinct(product)) as num_sold, GROUP_CONCAT( DISTINCT product 
-    ORDER BY product ASC 
-    separator ',') AS Products  from activities group by sell_date order by sell_date;
